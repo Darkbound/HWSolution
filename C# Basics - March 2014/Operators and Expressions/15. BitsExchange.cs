0f27bcf5ea50bbe2251afe3f949c83e01d465a80
Darkbound <vachev.t@gmail.com>
2014-05-17 12:53:39 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/Operators and Expressions/15. BitsExchange.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework
+{
+class BitsExchange
+{
+    static void Main()
+    {
+        Console.Write("Please input your number: ");
+        uint num = Convert.ToUInt32(Console.ReadLine());
+        uint numP = 0;
+        uint bit1 = 0;
+        uint bit2 = 0;
+        uint mask1 = 0;
+        uint mask2 = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            // get bit 3 (,4,5)
+            numP = num >> (3 + i);
+            bit1 = numP & 1;
+            // get bit 24 (,25,26)
+            numP = num >> (24 + i);
+            bit2 = numP & 1;
+            // shift bit 3 (,4,5) to positon 24 (,25,26)
+            bit1 = bit1 << (24 + i);
+            // shift bit 24 (,25,26) to position 3 (,4,5)
+            bit2 = bit2 << (3 + i);
+            // set bit 3 (,4,5) to zero
+            mask1 = (uint) 1 << (3 + i);
+            num = num & ~mask1;
+            // set bit 24 (,25,26) to zero
+            mask2 = (uint) 1 << (24 + i);
+            num = num & ~mask2;
+            // put bit 3 (,4,5) in bit 24 (,25,26)
+            num = num | bit1;
+            // put bit 24 (,25,26) in bit 3 (,4,5)
+            num = num | bit2;
+        }
+        Console.WriteLine("Your new number is: {0}", num);
+
+    }
+}
+}
---
+++ C# Basics - March 2014/Operators and Expressions/16. BitsExchangeAdv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Homework
+{
+class BitsExchangeAdv
+{
+    static void Main()
+    {
+        uint num = 0;
+        try
+        {
+            Console.Write("Please input your number: ");
+            num = Convert.ToUInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Out of Range!");
+        }
+        
+        Console.Write("How many bits do you want to exchange: ");
+        uint exchange = Convert.ToUInt32(Console.ReadLine());
+        Console.Write("Starting bit p: ");
+        uint p = Convert.ToUInt32(Console.ReadLine());
+        Console.Write("Starting bit q: ");
+        uint q = Convert.ToUInt32(Console.ReadLine());
+        uint numP = 0;
+        uint bit1 = 0;
+        uint bit2 = 0;
+        uint mask1 = 0;
+        uint mask2 = 0;
+
+        if (q + exchange > 31 || p+exchange > 31)
+	    {
+            Console.WriteLine("Out of range!");
+	    }
+        else
+	    {
+            if (p + exchange > q || q + exchange > p)
+            {
+                Console.WriteLine("Overlapping!");
+            }
+            else
+            {
+                for (int i = 0; i < exchange; i++)
+                {
+                    numP = num >> (int)(p + i);
+                    bit1 = numP & 1;
+                    numP = num >> (int)(q + i);
+                    bit2 = numP & 1;
+                    bit1 = bit1 << (int)(q + i);
+                    bit2 = bit2 << (int)(p + i);
+                    mask1 = (uint)1 << (int)(p + i);
+                    num = num & ~mask1;
+                    mask2 = (uint)1 << (int)(q + i);
+                    num = num & ~mask2;
+                    num = num | bit1;
+                    num = num | bit2;
+                }
+                Console.WriteLine("Your new number is: {0}", num);
+            }
+	    }
+    }
+}
+}
