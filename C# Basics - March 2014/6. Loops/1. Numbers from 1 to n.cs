5ba35f91fba128bf4a21d1fd9ad13dead886033b
Darkbound <vachev.t@gmail.com>
2014-05-21 01:41:43 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/1. Numbers from 1 to n.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class NumsFrom1toN
+    {
+        static void Main()
+        {
+            Console.Write("Print numbers from 1 to: ");
+            int n = int.Parse(Console.ReadLine());
+            string numbers = "";
+            for (int i = 1; i <= n; i++)
+            {
+                numbers += i.ToString() + " ";
+            }
+            Console.WriteLine("The numbers are: {0}", numbers);
+        }
+    }
+}
---
+++ C# Basics - March 2014/6. Loops/2. Not Divisible by 3 or 7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class NotDisivibleBy3or7
+    {
+        static void Main()
+        {
+            Console.Write("Numbers from 1 to: ");
+            int n = int.Parse(Console.ReadLine());
+            string numbers = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 != 0 && i % 7 != 0)
+                {
+                    numbers += i + " ";
+                }
+            }
+            Console.WriteLine(numbers);
+        }
+    }
+}
