9c8496a55786277517c004fec4ae1c5c3854c6b4
Darkbound <vachev.t@gmail.com>
2014-05-21 12:15:18 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/3. Min, Max, Sum and Average of N numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework_6
+{
+    class MinMaxSumAvg
+    {
+        static void Main()
+        {
+            List<double> list = new List<double>();
+
+            Console.Write("How many numbers are you going to input: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Input a number: ");
+                list.Add(double.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Min: " + list.Min());
+            Console.WriteLine("Max: " + list.Max());
+            Console.WriteLine("Sum: " + list.Sum());
+            Console.WriteLine("Average: " + list.Average());
+        }
+    }
+}
