e35e01e757a1b218bde95fdf89f853e2fe5d8af8
Darkbound <vachev.t@gmail.com>
2014-05-23 01:47:55 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/11. Random numbers in given range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class RandNumsInRange
+    {
+        static void Main()
+        {
+            Console.Write("How many random numbers do you want: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Min: ");
+            int min = int.Parse(Console.ReadLine());
+            Console.Write("Max: ");
+            int max = int.Parse(Console.ReadLine());
+            
+            Random rand = new Random();
+
+            for (int i = 0; i <= n; i++)
+            {
+                int num = rand.Next(min, max+1);
+                Console.Write("{0} ", num);
+            }
+        }
+    }
+}
