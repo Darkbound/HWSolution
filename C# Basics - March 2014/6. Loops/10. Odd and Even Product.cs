fa30a50f29ed741ef4e30a8fbfda0a24dee47ab9
Darkbound <vachev.t@gmail.com>
2014-05-22 20:45:19 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/10. Odd and Even Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class OddAndEvenproduct
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input your integers on a single line separated by a space(s): ");
+            string nums = Console.ReadLine();
+            double oddpro = 1;
+            double evenpro = 1;
+            nums = nums.Replace(" ", "");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((i + 1) % 2 == 1)
+                {
+                    oddpro = oddpro * double.Parse(nums[i].ToString());
+                }
+                if ((i + 1) % 2 == 0)
+                {
+                    evenpro = evenpro * double.Parse(nums[i].ToString());
+                }
+            }
+
+            if (oddpro == evenpro)
+            {
+                Console.WriteLine("Yes,\r\nProduct: {0}", oddpro);
+            }
+            else
+            {
+                Console.WriteLine("No,\r\nOdd Product: {0}\r\nEven Product: {1}", oddpro, evenpro);
+            }
+        }
+    }
+}
