aad93ee66e9345ae2bfe69a15df4f54d676b92ea
Darkbound <vachev.t@gmail.com>
2014-05-22 15:24:03 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/9. Matrix of Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class MatrixOfNums
+    {
+        static void Main()
+        {
+            Console.Write("Input the dimensions for the square matrix: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int j = i; j < n+i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
