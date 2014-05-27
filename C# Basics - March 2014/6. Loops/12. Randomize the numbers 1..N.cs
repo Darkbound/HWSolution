24ddf4685ddb1b177af18b552430cb08b3593b1e
Darkbound <vachev.t@gmail.com>
2014-05-27 21:49:19 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/12. Randomize the numbers 1..N.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_6
+{
+    class RandomizeNums1toN
+    {
+        static void Main()
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int[] newnums = new int[n];
+            int[] indexes = new int[n];
+            Random rand = new Random();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = rand.Next(0, n);
+
+                while (indexes.Contains(num))
+                {
+                    num = rand.Next(0, n);
+                }
+                
+                newnums[num] = numbers[i];
+                indexes[i] = num;
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
