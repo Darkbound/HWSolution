66d4e600a83f90d8e4ed1cb2d4ebc58f875b7b97
Darkbound <vachev.t@gmail.com>
2014-05-28 23:27:06 +0300

    C# Basics - March 2014 Solutions

---
+++ C# Basics - March 2014/6. Loops/13. Binary to Decimal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class BinaryToDecimal
+    {
+        static void Main()
+        {
+            Console.Write("Enter your number in binary format: ");
+            string binary = Console.ReadLine();
+            double check = 0;
+            double newnum = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                check = int.Parse(binary[binary.Length - i - 1].ToString());
+                if (check == 1)
+                {
+                    newnum += Math.Pow(2, i);
+                }
+
+            }
+            Console.WriteLine("Your number in decimal format is: {0}", newnum);
+        }
+    }
+}
